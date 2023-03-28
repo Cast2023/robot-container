95476e27a4c9e2e7a8ea569d657396b805c3b121
Helmi K <72031938+karhelmi@users.noreply.github.com>
2023-03-28 17:45:45 +0300

    Updated to equal tasks.py of the cast repo
    Modified to reflect the new folder structure of robottests

--- tasks.py
+++ tasks.py
@@ -1,10 +1,17 @@
 from invoke import task
 
-
 @task
 def test(ctx):
-    ctx.run("robot --argumentfile robottest/local-dev.args robottest")
+    ctx.run("robot --argumentfile variables/local-dev.args tests")
 
 @task
 def container_test(ctx):
-    ctx.run("robot --argumentfile robottest/container.args robottest")
+    ctx.run("robot --argumentfile variables/container.args tests")
+
+@task
+def dryrun_test(ctx):
+    ctx.run("robot --dryrun --argumentfile variables/local-dev.args tests")
+
+@task
+def dryrun_container_test(ctx):
+    ctx.run("robot --dryrun --argumentfile variables/container.args tests")
